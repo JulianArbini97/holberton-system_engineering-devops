7c5abc117027da59f80e434b35dd1c7e8522db85
JulianArbini97 <1890@holbertonschool.com>
2021-02-22 14:25:28 +0000

    done tasks mandatories

---
+++ 0x06-regular_expressions/0-simply_match_holberton.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+# Ruby script that takes argument and pass it to a matching method
+puts ARGV[0].scan(/Holberton/).join
---
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+# Ruby script that takes argument and pass it to a matching method
+puts ARGV[0].scan(/h[0-9a-zA-Z]n/).join
