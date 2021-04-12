fcfa3ec1d35b722238a2b2185776f32d839d5959
JulianArbini97 <1890@holbertonschool.com>
2021-04-12 23:45:11 +0000

    2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,6 +2,6 @@
 
 exec {'OneCommandInstallation':
   command  => 'sudo apt-get update -y; sudo apt-get install nginx -y; 
-  	       sudo sed -i "46i\add_header X-Served_By \$HOSTNAME;" /etc/nginx/sites-available/default; sudo service nginx restart',
+  	       sudo sed -i "46i\add_header X-Served-By \$HOSTNAME;" /etc/nginx/sites-available/default; sudo service nginx restart',
   provider => 'shell',
 }
