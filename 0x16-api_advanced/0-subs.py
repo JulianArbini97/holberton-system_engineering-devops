8bf2f6f871610e70885e6f14d2d48b5ffa954c64
JulianArbini97 <1890@holbertonschool.com>
2021-05-18 17:52:31 +0000

    Task 0 fix, User-Agent added

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,8 +6,8 @@ import requests
 def number_of_subscribers(subreddit):
     """ Function that returns number of subs """
     page = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    req = requests.get(page)
+    req = requests.get(page, headers={"User-Agent": "Mozilla/5.0"})
     json_req = req.json()
-    if json_req.get('error') == 404:
+    if req.status_code != 200:
         return (0)
     return json_req.get('data').get('subscribers')
