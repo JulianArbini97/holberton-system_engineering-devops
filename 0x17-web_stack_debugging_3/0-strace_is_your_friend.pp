5dad039f70d8f4aee745e125086191dd81419224
JulianArbini97 <70916343+JulianArbini97@users.noreply.github.com>
2021-05-19 17:10:54 -0300

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Comment line
-exec { 'wsd3':
+exec { 'bla':
   command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/' /var/www/html/wp-settings.php",
   path    =>  ['/bin', '/usr/bin']
 }
