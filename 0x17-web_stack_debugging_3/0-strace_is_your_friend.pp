799e267105ea972533ef83ee09d0c663e81792f2
JulianArbini97 <1890@holbertonschool.com>
2021-05-20 02:42:40 +0000

    Task 0 fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
-# Comment line
+# Using strace, find out why Apache is returning a 500 error. 
 exec { 'func':
   command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/' /var/www/html/wp-settings.php",
-  provider =>  'shell',
+  path    =>  ['/bin', '/usr/bin']
 }
